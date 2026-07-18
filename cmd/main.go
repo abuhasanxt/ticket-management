865752b16a1926168ad39b75ca46b550c47545de
unknown <abuhasanxt@gmail.com>
2026-07-18 20:11:26 +0600

    implementing request validation

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,58 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/labstack/echo/v5"
-  "github.com/labstack/echo/v5/middleware"
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
+type Users struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally return the error to let each route control the status code.
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
 func main() {
-  e := echo.New()
+	e := echo.New()
 
-  e.Use(middleware.RequestLogger())
-  e.Use(middleware.Recover())
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
 
-  e.GET("/", func(c *echo.Context) error {
-    return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
-  })
+	e.GET("/", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
+	})
+	e.POST("/users", func(c *echo.Context) error {
+		u := new(Users)
 
-  if err := e.Start(":8080"); err != nil {
-    e.Logger.Error("failed to start server", "error", err)
-  }
+		//binding the user data
+		if err := c.Bind(u); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+
+		}
+		//validating the user data
+		if err := c.Validate(u); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+
+		//process the user data (e.g,save the database)
+		return c.JSON(http.StatusCreated, u)
+	})
+
+	if err := e.Start(":8080"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
 }
